f035f8e6a398c46ea0161d3b408ce163acdd1e8c
Agnes Jardeleza <paraholic.ablaze@gmail.com>
2017-12-05 21:10:47 +0800

    Added simple javascript lightbox - to be converted to jquery - to product page

--- js/main.js
+++ js/main.js
@@ -19,3 +19,37 @@ $(document).on('click', '.js-cart-open', function() {
         });
     }
 });
+
+// Lightbox from W3 Schools 
+// https://www.w3schools.com/howto/howto_js_lightbox.asp
+// TODO: convert to jquery
+
+function openModal() {
+  document.getElementById('lightbox').style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById('lightbox').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
